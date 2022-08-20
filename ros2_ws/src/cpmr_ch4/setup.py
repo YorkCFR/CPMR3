4a8cfd0de1945649743f1ed47673506c043caf09
Michael Jenkin <michaeljenkin@me.com>
2022-08-19 23:39:58 -0400

    ch4

--- ros2_ws/src/cpmr_ch4/setup.py
+++ ros2_ws/src/cpmr_ch4/setup.py
@@ -1,15 +1,21 @@
+import os
+from glob import glob
 from setuptools import setup
+
 
 package_name = 'cpmr_ch4'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
