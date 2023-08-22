037c75472a4d71217f63aaa13529b9970ab75ade
Michael Jenkin <jenkin@yorku.ca>
2023-08-22 18:29:11 -0400

    adding chapter 11

---
+++ ros2_ws/src/cpmr_ch11/launch/chairs.launch.py
@@ -0,0 +1,61 @@
+import os
+import json
+import sys
+import math
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, LogInfo
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+    nchairs = 5
+    convoy_file = 'convoy.json'
+    for arg in sys.argv: # there must be a better way...
+        if arg.startswith('nchairs:='):
+           print(arg.split('chairs:=', 1)[1])
+           nchairs = int(arg.split('chairs:=', 1)[1])
+        elif ':=' in arg:
+           print(f"Unknown argument in {arg}")
+           sys.exit(0)
+    print(f"Launching {nchairs}")
+
+    nodelist = []
+
+    # fire up gazebo
+    gazebo = os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+    q = IncludeLaunchDescription(gazebo)
+    nodelist.append(q)
+
+    # process each chair
+    urdf = os.path.join(get_package_share_directory('cpmr_ch11'), 'wheelchair.urdf.xacro')
+
+    for chair in range(nchairs):
+        x = 5 * math.cos(chair * math.pi * 2 / nchairs)
+        y = 5 * math.sin(chair * math.pi * 2 / nchairs)
+        name = f'chair_{chair}'
+        print(f"Processing {chair}")
+        robot_desc = xacro.process_file(urdf, mappings={'name' : name, 'show_video': 'True', 'camera_tilt': '0.25'}).toxml()
+        nodelist.append(
+            Node(
+                namespace = name,
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{'use_sim_time': False, 'robot_description': robot_desc, 'frame_prefix': name + "/"}],
+                arguments=[urdf])
+            )
+        nodelist.append(
+            Node(
+                namespace = name,
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                name='urdf_spawner',
+                output='screen',
+                arguments=["-topic", "/" + name + "/robot_description",  "-entity",  name, "-x", str(x), '-y', str(y), '-Y', '0'])
+            )
+    return LaunchDescription(nodelist)
---
+++ ros2_ws/src/cpmr_ch11/setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'cpmr_ch11'
+
+setup(
+    name=package_name,
+    version='0.3.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jenkin',
+    maintainer_email='jenkin@yorku.ca',
+    description='PMR 3rd Edition Chapter 11',
+    license='MIT License',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
