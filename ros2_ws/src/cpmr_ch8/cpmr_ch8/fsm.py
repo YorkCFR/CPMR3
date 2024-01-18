6167e9245d74addf4267211c7f5b8add8f00e711
Michael Jenkin <jenkin@yorku.ca>
2024-01-18 10:32:52 -0500

    tweaks to the fsm

--- ros2_ws/src/cpmr_ch8/cpmr_ch8/fsm.py
+++ ros2_ws/src/cpmr_ch8/cpmr_ch8/fsm.py
@@ -54,52 +54,59 @@ class FSM(Node):
         self._cur_theta = 0.0
         self._cur_state = FSM_STATES.AT_START
         self._start_time = self.get_clock().now().nanoseconds * 1e-9
+        self._points = [[0, 0, 0], [0, 5, 0], [5, 5, math.pi/2], [5, 0, -math.pi/2]]
+        self._point = 0
 
+    def _short_angle(angle):
+        if angle > math.pi:
+            angle = angle - 2 * math.pi
+        if angle < -math.pi:
+            angle = angle + 2 * math.pi
+        assert abs(angle) <= math.pi
+        return angle
+
+    def _compute_speed(diff, max_speed, min_speed, gain):
+        speed = abs(diff) * gain
+        speed = min(max_speed, max(min_speed, speed))
+        return math.copysign(speed, diff)
+        
     def _drive_to_goal(self, goal_x, goal_y, goal_theta,
-                       heading0_tol = 0.1,
-                       heading0_v = 0.05,
-                       range_tol = 0.1,
-                       range_v = 0.1,
-                       heading1_tol = 0.1,
-                       heading1_v = 0.05):
+                       heading0_tol = 0.15,
+                       range_tol = 0.15,
+                       heading1_tol = 0.15):
         """Return True iff we are at the goal, otherwise drive there"""
 
-        self.get_logger().info(f'{self.get_name()} drive to goal')
         twist = Twist()
+
+        goal_theta = FSM._short_angle(goal_theta) # goal in range -pi .. +pi
 
         x_diff = goal_x - self._cur_x
         y_diff = goal_y - self._cur_y
         dist = math.sqrt(x_diff * x_diff + y_diff * y_diff)
-
-        # turn to the goal
-        heading = math.atan2(y_diff, x_diff)
-        diff = heading - self._cur_theta
-        if diff > math.pi:
-            diff = diff - 2 * math.pi
-        if diff < -math.pi:
-            diff = diff + 2 * math.pi
-        if abs(diff) > heading0_tol:
-            twist.angular.z = math.copysign(heading0_v, diff)
-            self.get_logger().info(f'{self.get_name()} turning towards goal diff {diff} {twist.angular.z}')
-            self._publisher.publish(twist)
-            return False
-
-        # pointing the right direction, so go there
         if dist > range_tol:
-            twist.linear.x = range_v
+            self.get_logger().info(f'{self.get_name()} driving to goal with goal distance {dist}')
+            # turn to the goal
+            heading = math.atan2(y_diff, x_diff)
+            diff = FSM._short_angle(heading - self._cur_theta)
+            if (abs(diff) > heading0_tol):
+                twist.angular.z = FSM._compute_speed(diff, 0.5, 0.2, 0.2)
+                self.get_logger().info(f'{self.get_name()} turning towards goal heading {heading} current {self._cur_theta} diff {diff} {twist.angular.z}')
+                self._publisher.publish(twist)
+                self._cur_twist = twist
+                return False
+
+            twist.linear.x = FSM._compute_speed(dist, 0.5, 0.05, 0.5)
             self._publisher.publish(twist)
-            self.get_logger().info(f'{self.get_name()} driving to goal dist {dist} {twist.linear.x}')
+            self.get_logger().info(f'{self.get_name()} a distance {dist}  from target velocity {twist.linear.x}')
+            self._cur_twist = twist
             return False
 
         # we are there, set the correct angle
-        diff = goal_theta - self._cur_theta
-        if diff > math.pi:
-            diff = diff - 2 * math.pi
-        if diff < -math.pi:
-            diff = diff + 2 * math.pi
+
+        diff = FSM._short_angle(goal_theta - self._cur_theta)
         if abs(diff) > heading1_tol:
-            twist.angular.z = math.copysign(heading1_v, diff)
-            self.get_logger().info(f'{self.get_name()} turning to goal {goal_theta} {twist.angular.z}')
+            twist.angular.z = FSM._compute_speed(diff, 0.5, 0.2, 0.2)
+            self.get_logger().info(f'{self.get_name()} aligning with goal dst {dist} goal_theta {goal_theta} cur_theta {self._cur_theta} diff {diff} {twist.angular.z}')
             self._publisher.publish(twist)
             return False
 
@@ -115,13 +122,16 @@ class FSM(Node):
             self._cur_state = FSM_STATES.HEADING_TO_TASK
 
     def _do_state_heading_to_task(self):
-        self.get_logger().info(f'{self.get_name()} heading to task {self._cur_x} {self._cur_y} {self._cur_theta}')
-        if self._drive_to_goal(2, 1, math.pi/2, heading1_tol = math.pi * 2): # don't care about final orientation
-            self._cur_state = FSM_STATES.RETURNING_FROM_TASK
+        self.get_logger().info(f'{self.get_name()} heading to task {self._point}')
+        if self._drive_to_goal(self._points[self._point][0], self._points[self._point][1], self._points[self._point][2]): 
+            self._point = self._point + 1
+            if self._point >= len(self._points):
+                self._cur_state = FSM_STATES.RETURNING_FROM_TASK
 
     def _do_state_returning_from_task(self):
         self.get_logger().info(f'{self.get_name()} returning from task ')
         if self._drive_to_goal(0, 0, 0):
+            self._publisher.publish(Twist())
             self._cur_state = FSM_STATES.TASK_DONE
 
     def _do_state_task_done(self):
@@ -145,7 +155,7 @@ class FSM(Node):
         roll, pitch, yaw = euler_from_quaternion(pose.orientation)
         self._cur_x = pose.position.x
         self._cur_y = pose.position.y
-        self._cur_theta = yaw
+        self._cur_theta = FSM._short_angle(yaw)
         self._state_machine()
 
 
@@ -155,9 +165,9 @@ def main(args=None):
     node = FSM()
     try:
         rclpy.spin(node)
+        rclpy.shutdown()
     except KeyboardInterrupt:
         pass
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
